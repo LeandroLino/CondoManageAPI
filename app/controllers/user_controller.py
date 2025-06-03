8e2928dd75aa426f3d2c87d825d328cc00a119d8
LeandroLino <leao.lino7@gmail.com>
2025-06-03 19:00:45 -0300

    Feature :: added token in coockies

--- app/controllers/user_controller.py
+++ app/controllers/user_controller.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Response
 from sqlalchemy.orm import Session
 from app.database.connection import get_db
 from app.services.user_service import UserService
@@ -9,17 +9,15 @@ from app.core.config import settings
 user_router = APIRouter(prefix="/user")
 
 @user_router.post("/register")
-def register_user(user: UserRegisterRequest, db: Session = Depends(get_db)):
-    result = UserService.register_user(user.name, user.email, user.document_number, user.password, user.role, db)
-    print("Result: ",result)
+def register_user(user: UserRegisterRequest, db: Session = Depends(get_db), response: Response = None):
+    result = UserService.register_user(user.name, user.email, user.document_number, user.password, db, user.role, response)
     if "error" in result:
         return {"result": "Failed", "message": result["error"], 'token': None}
     return {'token': result}
 
 @user_router.post("/login")
-def register_user(user: UserLoginRequest, db: Session = Depends(get_db)):
-    result = UserService.login_user(user.email, user.password, db)
-    print(result)
+def login_user(user: UserLoginRequest, db: Session = Depends(get_db), response: Response = None):
+    result = UserService.login_user(user.email, user.password, db, response)
     if "error" in result:
         return {"result": "Failed", "message": result["error"], 'token': None}
     return {'token': result}
--- app/services/user_service.py
+++ app/services/user_service.py
@@ -4,10 +4,22 @@ from datetime import datetime, timedelta
 from app.models.user import UserModel
 from sqlalchemy.orm import Session
 from app.core.config import settings
+from fastapi import Depends, Response, HTTPException
+from fastapi.exceptions import HTTPException
+from jwt import ExpiredSignatureError, DecodeError
 
+def verify_refresh_token(refresh_token: str = Depends()):
+    try:
+        # Decodificando o refresh token
+        payload = jwt.decode(refresh_token, settings.jwt_secret_key, algorithms=["HS256"])
+        return payload
+    except ExpiredSignatureError:
+        raise HTTPException(status_code=401, detail="Refresh token expirado.")
+    except DecodeError:
+        raise HTTPException(status_code=401, detail="Refresh token inválido.")
 class UserService:
     @staticmethod
-    def register_user(name: str, email: str, document_number: str, password: str, role: str = 'visitor', db: Session = None):
+    def register_user(name: str, email: str, document_number: str, password: str, db: Session = None, role: str = 'visitor', response: Response = None):
         # Hash da senha antes de armazená-la
         hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
 
@@ -34,35 +46,59 @@ class UserService:
         db.commit()
         db.refresh(user)
 
+        access_token = UserService.generate_jwt_token(user.id, user.role)
+        
+        refresh_token = UserService.generate_refresh_token(user.id)
+
+        response.set_cookie(
+            key="refresh_token",
+            value=refresh_token,
+            httponly=True,  # Protege contra XSS
+            secure=True,  # Assegura que só seja transmitido por HTTPS
+            samesite="Strict",  # Protege contra CSRF
+            max_age=30 * 24 * 60 * 60  # Expira em 30 dias
+        )
         # Gerar o token JWT
-        token = UserService.generate_jwt_token(user.id, user.role)
 
         # Retorna o ID do usuário registrado e o token
-        return {"message": "Usuário registrado com sucesso", "user_id": user.id, "token": token}
+        return {"token": access_token}
 
     @staticmethod
-    def login_user(email: str, password: str, db: Session):
+    def login_user(email: str, password: str, db: Session, response: Response):
         # Buscar o usuário pelo email
         user = db.query(UserModel).filter(UserModel.email == email).first()
         
         # Verificar se o usuário existe
         if not user:
             return {"error": "Usuário não encontrado."}
-        print("Testando: ", user.password_hash)
+        
         # Verificar se a senha fornecida é válida
         if not bcrypt.checkpw(password.encode('utf-8'), user.password_hash.encode('utf-8')):
             return {"error": "Senha incorreta."}
         
-        # Gerar um token JWT
-        token = UserService.generate_jwt_token(user.id, user.role)
+        # Gerar o token JWT (access token)
+        access_token = UserService.generate_jwt_token(user.id, user.role)
         
-        # Retornar o token de autenticação
-        return {"message": "Login bem-sucedido", "token": token}
+        # Gerar o refresh token de longa duração
+        refresh_token = UserService.generate_refresh_token(user.id)
+
+        # Armazenando o refresh token em um cookie HttpOnly
+        response.set_cookie(
+            key="refresh_token",
+            value=refresh_token,
+            httponly=True,  # Protege contra XSS
+            secure=True,  # Assegura que só seja transmitido por HTTPS
+            samesite="Strict",  # Protege contra CSRF
+            max_age=30 * 24 * 60 * 60  # Expira em 30 dias
+        )
+        
+        # Retorna o access token diretamente na resposta
+        return {"access_token": access_token}
     
     @staticmethod
     def generate_jwt_token(user_id: int, role) -> str:
         role_value = role.value if hasattr(role, 'value') else role
-        expiration_time = datetime.utcnow() + timedelta(hours=1)
+        expiration_time = datetime.utcnow() + timedelta(hours=1)  # Expiração do access token em 1 hora
         payload = {
             "user_id": user_id,
             "role": role_value,
@@ -71,3 +107,30 @@ class UserService:
         
         token = jwt.encode(payload, settings.jwt_secret_key, algorithm="HS256")
         return token
+
+    @staticmethod
+    def generate_refresh_token(user_id: int) -> str:
+        expiration_time = datetime.utcnow() + timedelta(days=30)  # Expira em 30 dias
+        payload = {
+            "user_id": user_id,
+            "exp": expiration_time
+        }
+        
+        # Gerando o refresh token
+        refresh_token = jwt.encode(payload, settings.jwt_secret_key, algorithm="HS256")
+        return refresh_token
+
+    @staticmethod
+    def refresh_access_token(response: Response, db: Session, refresh_token: str = Depends(verify_refresh_token)):
+        # O refresh_token é verificado e extraído da função verify_refresh_token
+        
+        # Buscar o usuário do banco usando o user_id do refresh token
+        user = db.query(UserModel).filter(UserModel.id == refresh_token["user_id"]).first()
+        if not user:
+            raise HTTPException(status_code=401, detail="Usuário não encontrado.")
+
+        # Gerar um novo access token
+        access_token = UserService.generate_jwt_token(user.id, user.role)
+        
+        return {"access_token": access_token}
+
